beb1c9b131609217d3f6c69364227e9897e283d9
AnggaNaufal <angganaufal27@gmail.com>
2026-03-09 11:37:28 +0700

    Create landing-page.php

---
+++ landing-page.php
@@ -0,0 +1,37 @@
+<?php
+
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <div class="container">
+        <div class="header">
+
+        </div>
+    </div>
+    <div class="sidebar">
+      <div class="logo">
+        <img src="asset/logo.svg" alt="" />
+      </div>
+      <div class="konten">
+        <ul>
+
+          <li><img src="asset/ktp.svg" alt="">Ambil Antrian</li>
+          <li>Kartu Antrian</li>
+          <li>YOYOKS KASNURS HELAMBANGS</li>
+          
+        </ul>
+      </div>
+    </div>
+  </body>
+</html>
+
+
+
