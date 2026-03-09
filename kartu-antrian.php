2da80b021de83dd901b20bb3007aa7ee22cb4dd3
GadzDep <gadingartaa@gmail.com>
2026-03-09 11:57:13 +0700

    Update kartu-antrian.php

--- kartu-antrian.php
+++ kartu-antrian.php
@@ -2,12 +2,32 @@
 
 ?>
 
-<!DOCTYPE html>
-<html>
-<body>
-    <h1>Halo, ini HTML</h1>
-    <?php
-        echo "<p>Ini dihasilkan oleh PHP</p>";
-    ?>
-</body>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <div class="container">
+        <div class="header">
+
+        </div>
+    </div>
+    <div class="sidebar">
+      <div class="logo">
+        <img src="asset/logo.svg" alt="" />
+      </div>
+      <div class="konten">
+        <ul>
+
+          <li><img src="asset/ktp.svg" alt="">Ambil Antrian</li>
+          <li>Kartu Antrian</li>
+          <li>Daftar Antrian</li>
+        </ul>
+      </div>
+    </div>
+  </body>
 </html>
